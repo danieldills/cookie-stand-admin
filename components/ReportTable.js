01481c5618ebe7f033d07f2cbe135efb27cd05ff
Daniel Dills <dills253@gmail.com>
2021-10-02 17:58:45 -0700

    created form handler function

--- components/ReportTable.js
+++ components/ReportTable.js
@@ -1,6 +1,18 @@
 import React from "react";
 
 function ReportTable() {
+  /*
+
+  Expecting Props
+  - hours props -> an array of cookie stand hours(data.js)
+  - reports props -> array of all cookie stand objects
+
+  - if reports is empty, render <h2> no cookie stands <h2>
+  - otherwise render table
+  - need to tally totals for each cookie stand
+  - need to tally totals for per hourly slot
+  */
+
   return (
     <div>
       <h1>ReportTable</h1>
