ef12000db7cb55eb78304c1ddf411fc7f0568f31
Daniel Dills <dills253@gmail.com>
2021-10-06 18:00:36 -0700

    second commit

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 function LoginForm() {
+  const [formItems, setFormItems] = useState({});
+  console.log("FormItems: ", formItems);
+
+  const handleChange = (e) => {
+    const newFormItems = {
+      ...formItems,
+      [e.target.name]: e.target.value,
+    };
+    setFormItems(newFormItems);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("this was clicked");
+  };
+
   return (
-    <div className="w-1/2 p-8 mx-auto my-16 text-center bg-green-400">
+    <form
+      onSubmit={handleSubmit}
+      className="w-1/2 p-8 mx-auto my-16 text-center bg-green-400 rounded-lg"
+    >
       <h3>Username</h3>
-      <input name="email" type="email" className="w-3/4" />
+      <input
+        name="email"
+        type="email"
+        className="w-3/4 rounded-sm"
+        onChange={handleChange}
+      />
       <h3 className="pt-8">Password</h3>
-      <input name="password" type="password" className="w-3/4" />
-      <button className="w-1/3 p-3 mt-8 bg-green-200">Sign In</button>
-    </div>
+      <input
+        name="password"
+        type="password"
+        className="w-3/4 rounded-sm"
+        onChange={handleChange}
+      />
+      <button className="w-1/3 p-3 mt-8 bg-green-200 rounded-md">
+        Sign In
+      </button>
+    </form>
   );
 }
 
